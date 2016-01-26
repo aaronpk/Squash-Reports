7479d8ff8c2bc43aaa8c9ddcbf5645917bc9b067
Aaron Parecki <aaron@parecki.com>
2016-01-26 15:20:31 -0800

    fix emoji migration

--- squash/database/migrations/2016_01_01_000001_import_emoji.php
+++ squash/database/migrations/2016_01_01_000001_import_emoji.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Initialize extends Migration
+class ImportEmoji extends Migration
 {
     /**
      * Run the migrations.
