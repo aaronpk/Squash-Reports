fba7c60995ec29251b6632275de283a2d0b7fee2
Aaron Parecki <aaron@parecki.com>
2019-01-06 19:11:42 -0800

    show year when viewing older entries

--- squash/app/Providers/AppServiceProvider.php
+++ squash/app/Providers/AppServiceProvider.php
@@ -18,7 +18,12 @@ class AppServiceProvider extends ServiceProvider
         return '<?php
           $date = new DateTime($entry->created_at);
           $date->setTimeZone(new DateTimeZone($entry->timezone));
-          echo $date->format(\'M j g:ia\'); ?>';
+          $now = new DateTime();
+          if($now->format(\'Y\') != $date->format(\'Y\'))
+            $format = \'M j, Y g:ia\';
+          else
+            $format = \'M j g:ia\';
+          echo $date->format($format); ?>';
       });
 
       Blade::directive('entrydateforgroup', function($entry) {
